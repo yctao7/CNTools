ea213f9652c6de68f67c91e44cec314b4100aeb1
Yicheng Tao <yctao@umich.edu>
2022-11-06 15:17:01 -0500

    Modify directory names

--- run_identify.sh
+++ run_identify.sh
@@ -1,30 +1,30 @@
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 5
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 8
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 10
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 15
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 20
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 5
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 8
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 10
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 15
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CC --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CC --m 20
 
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CFIDF --n_cns 6 --cns_path CN/T2D/CFIDF/cns_original.pkl --Naive 3 --HMRF 45 9 --verbose
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CFIDF --n_cns 6 --cns_path cn/T2D/CFIDF/cns_original.pkl --Naive 3 --HMRF 45 9 --verbose
 
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 1
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 2
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 3
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 4
-# python identify.py --ds_path Data/T2D/T2D_ds.pkl --out_dir CN/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 5
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 1
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 2
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 3
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 4
+# python identify.py --ds_path data/T2D/T2D_ds.pkl --out_dir cn/T2D/CNE --n_cns 6 --Naive 3 --HMRF 45 9 --seed 0 --verbose CNE --eta 5
 
 
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 0.5
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 0.8
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 1.0
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 0.5
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 0.8
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 1.0
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 0.5
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 0.8
-# python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 1.0
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 0.5
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 0.8
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 32 --r 1.0
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 0.5
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 0.8
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 40 --r 1.0
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 0.5
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 0.8
+# python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CFIDF --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CFIDF --eps 50 --r 1.0
 
-python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 1
-python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 2
-python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 3
-python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 4
-python identify.py --ds_path Data/CRC/CRC_ds.pkl --out_dir CN/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 5
+python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 1
+python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 2
+python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 3
+python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 4
+python identify.py --ds_path data/CRC/CRC_ds.pkl --out_dir cn/CRC/CNE --n_cns 9 --Naive 3 --HMRF 40 9 --seed 0 --verbose CNE --eta 5
--- run_load.sh
+++ run_load.sh
@@ -1,3 +1,3 @@
-python load.py --df_path Data/CRC/CRC_clusters_neighborhoods_markers.csv --df_name CRC --out_dir Data/CRC --ct_order_path Data/CRC/ct_order.json
-python load.py --df_path Data/T2D/Cell-ID_by-islet.csv --df_name T2D --out_dir Data/T2D --ct_order_path Data/T2D/ct_order.json
-# python load.py --df_path Data/HLT/Lymphoid_Data.csv --df_name HLT --out_dir Data/HLT
+python load.py --df_path data/CRC/CRC_clusters_neighborhoods_markers.csv --df_name CRC --out_dir Data/CRC --ct_order_path data/CRC/ct_order.json
+python load.py --df_path data/T2D/Cell-ID_by-islet.csv --df_name T2D --out_dir Data/T2D --ct_order_path data/T2D/ct_order.json
+python load.py --df_path data/HLT/Lymphoid_Data.csv --df_name HLT --out_dir data/HLT
